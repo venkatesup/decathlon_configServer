f3027c3c79f799fe9296f859e558d7dc1b273bc9
venkata <venky.pusarla47@gmail.com>
2019-01-24 22:57:38 +0530

    Recoding

--- decathlongConfigServer/src/main/java/com/decathlon/configserver/DecathlongConfigServerApplication.java
+++ decathlongConfigServer/src/main/java/com/decathlon/configserver/DecathlongConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.decathlon.microservices;
+package com.decathlon.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
